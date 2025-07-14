7a31915b055be5e65eb3a54d44c66ed915717d04
lam <lam@login-b.zuvela.ibm.com>
2025-07-14 17:36:44 +0200

    Linting

--- examples/aihwkit_evaluation/evaluation.py
+++ examples/aihwkit_evaluation/evaluation.py
@@ -1,13 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2024 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Example on how to evaluate an AIHWKIT-Lightning model with AIHWKIT."""
+
 import torch
-from model import resnet32
-from aihwkit_lightning.simulator.configs import (
-    TorchInferenceRPUConfig,
-)
-from aihwkit_lightning.nn.conversion import convert_to_analog
-from aihwkit_lightning.nn.export import export_to_aihwkit
-from aihwkit.simulator.configs import InferenceRPUConfig
 from aihwkit.inference.noise.pcm import PCMLikeNoiseModel
 from aihwkit.inference.compensation.drift import GlobalDriftCompensation
+from model import resnet32
+from aihwkit_lightning.simulator.configs import TorchInferenceRPUConfig
+from aihwkit_lightning.nn.conversion import convert_to_analog
+from aihwkit_lightning.nn.export import export_to_aihwkit
 
 if __name__ == "__main__":
     model = resnet32()
--- examples/aihwkit_evaluation/model.py
+++ examples/aihwkit_evaluation/model.py
@@ -1,13 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# (C) Copyright 2024 IBM. All Rights Reserved.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Resnet32 model definition."""
+
+# pylint: skip-file
+
 import torch
 import torch.nn.functional as F
-import torch.nn.init as init
-import numpy as np
+from torch.nn import init
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def _weights_init(m):
-    if isinstance(m, torch.nn.Linear) or isinstance(m, torch.nn.Conv2d):
+    if isinstance(m, (torch.nn.Linear, torch.nn.Conv2d)):
         init.kaiming_normal_(m.weight)
 
 
